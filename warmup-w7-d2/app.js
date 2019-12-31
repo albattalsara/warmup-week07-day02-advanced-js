d07c8eb246a7b5bded425e11e052626a34010b29
AlBattalSara <YOUR GITHUB EMAIL>
2019-12-31 19:13:50 +0300

    API homework memes

--- warmup-w7-d2/app.js
+++ warmup-w7-d2/app.js
@@ -1,1 +1,22 @@
 // code goes here
+
+const URL_MEME = "https://api.imgflip.com/get_memes"; 
+
+let response = {}
+function memes(){
+    axios.get(URL_MEME).then(function(show){
+    
+        response= show.data;
+        // response= response.data.memes;
+        console.log(response);
+        response.data.memes.forEach(element => { 
+            
+            document.getElementById("ID").innerHTML +=` <div>${element.name}</div> <img src= ${element.url} width=${element.width} height=${element.height} >`
+            
+        });
+     
+        
+        
+    });
+
+}
